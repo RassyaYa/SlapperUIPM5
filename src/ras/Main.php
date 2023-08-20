b35a3224385d79516bcfb1e7d5b8e73532b5dd53
RassyaYa <117354752+RassyaYa@users.noreply.github.com>
2023-08-20 08:59:51 +0700

    Update Main.php

--- src/ras/Main.php
+++ src/ras/Main.php
@@ -9,3 +9,13 @@ use pocketmine\player\Player;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
+
+use ras\Forms\SimpleForm;
+use ras\Forms\FormUI;
+use ras\Forms\Form;
+
+class main extends PluginBase implements Listener {
+
+    public function onEnable() : void {
+        $this->getLogger()->info(tf::GREEN. "Enable Tutorial");
+    }
