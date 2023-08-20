71515538e6e3adad1e6e91bde2dca9a41ad94806
RassyaYa <117354752+RassyaYa@users.noreply.github.com>
2023-08-20 08:25:28 +0700

    Create Main.php

---
+++ src/ras/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ras;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server
+use pocketmine\player\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
